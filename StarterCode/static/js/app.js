674b7c909a4ba1cbae22eaa7e72d5f6037f04452
mpagerit <65865133+mpagerit@users.noreply.github.com>
2020-10-01 18:09:22 -0500

    started bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,19 +1,42 @@
+// verify the correct .js file is being referenced
 console.log("This is app.js");
 
+// create a function to draw the bar graph
 function DrawBargraph(sampleID) {
     console.log(`DrawBargraph(${sampleID})`);
+
+    var trace1 = {
+        x: sampleID.map(row => row.sample_values),
+        y: sampleID.map(row => row.otu_ids),
+        text: sampleID.map(row => row.otu_ids),
+        name: "OTU",
+        type: "bar",
+        orientation: "h" 
+    };
 }
 
+// create a function to draw the bubble chart
+function DrawBubblechart(sampleID) {
+    console.log(`DrawBubblechart(${sampleID})`);
+}
+
+function populateDemographicInfo(sampleID) {
+    console.log(`Demographic info for ${sampleID}`);
+}
+
+// create a function that will update the graph and the chart based on a new selection
 function optionChanged(newSampleID) {
     console.log(`User selected ${newSampleID}`);
 
     DrawBargraph(newSampleID);
+    DrawBubblechart(newSampleID);
+    populateDemographicInfo(newSampleID);
 }
 
 function InitDashboard()
 {
     console.log('calling InitDashboard');
-
+    // select the selector ID
     var selector = d3.select("#selDataset");
 
     d3.json("samples.json").then((data) => {
@@ -21,7 +44,7 @@ function InitDashboard()
 
         var sampleNames = data.names;
 
-        // populate hte slector with all sample IDs
+        // populate the slector with all sample IDs
         sampleNames.forEach((sampleID) => {
             selector.append("option")
                 .text(sampleID)
@@ -32,7 +55,8 @@ function InitDashboard()
         console.log("Starting Sample: ", sampleID);
 
         DrawBargraph(sampleID);
-        // DrawBubblechart(sampleID);
+        DrawBubblechart(sampleID);
+        populateDemographicInfo(sampleID);
     });
 }
 
